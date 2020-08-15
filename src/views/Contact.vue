ea6c021e6347d85f6aeba2da569bbfcd81bb2918
Joep Verhoeven <45588540+BluRRayS@users.noreply.github.com>
2020-08-15 11:31:18 +0200

    Added content to the about page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,13 +1,22 @@
 <template>
-<v-container><h1>Contact</h1></v-container>
+  <div>
+    <v-img src="@/assets/pellet.jpg" aspect-ratio="4.7" style="opacity:0.9">
+    <v-row align="top" class="lightbox white--text pa-2 fill-height">
+          <v-col>
+              <div class="text-h3 pl-4 font-weight-bold" light>Contact</div>
+          </v-col>
+        </v-row>
+        
+    </v-img>
+    <v-container>
+     
+    </v-container>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
